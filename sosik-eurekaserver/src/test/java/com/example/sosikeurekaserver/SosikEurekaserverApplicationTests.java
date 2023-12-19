8eae99ca5a44f0d528b079e99509159bf3f8bfe0
ComNewbie <iforking3@naver.com>
2023-12-19 12:31:45 +0900

    #Feat 초기 백엔드 개발 유레카서버 커밋

---
+++ sosik-eurekaserver/src/test/java/com/example/sosikeurekaserver/SosikEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sosikeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosikEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
